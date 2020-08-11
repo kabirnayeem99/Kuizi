f0371368a110743440431b33065233a0647a8923
Naimul Kabir <kabirnayeem.99@gmail.com>
2020-08-11 14:41:04 +0600

    merged

--- test/features/quiz_fetch_n_evaluation/data/result_model_test.dart
+++
@@ -1,7 +0,0 @@
-// import 'package:flutter_quiz_app/features/quiz_fetch_n_evaluation/data/model/result_model.dart';
-// import 'package:flutter_quiz_app/features/quiz_fetch_n_evaluation/domain/entity/result.dart';
-// import 'package:mockito/mockito.dart';
-
-// class MockResults extends Mock implements Results {}
-
-// class Mock {}
